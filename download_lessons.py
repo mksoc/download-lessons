24887a4c2647d06aa21385b912df0cf7f2787112
Marco Andorno <mksoc@users.noreply.github.com>
2017-04-23 16:45:44 +0200

    Add functions and exceptions

--- download_lessons.py
+++ download_lessons.py
@@ -2,6 +2,26 @@ from selenium import webdriver
 import subprocess
 import sys
 
+#--- Function definitions ---
+def open_page(link):
+    print('Opening browser and course page...')
+    driver.get(link)
+    try:
+        assert "Lezione" in driver.title
+    except AssertionError:
+        print('ERROR. Wrong page title. Check provided link.')
+        raise
+        
+def get_lesson_list():
+    lessons = driver.find_elements_by_partial_link_text('Lezioni')
+    try:
+        assert len(lessons) > 0
+        print('Found {} lessons.'.format(len(lessons)))
+    except AssertionError:
+        print('ERROR. No lesson found. Check provided link.')
+        raise
+#--- End function definitions ---
+
 # Define the url of the course and output directory
 course_url = 'https://elearning.polito.it/gadgets/video/template_video.php?utente=S213513&inc=202712&data=220420171728&token=926189389C4432FEAB407397FCB6A60B'
 output_dir = '/mnt/d/videolezioni/misure'
@@ -9,18 +29,15 @@ phantomjs_dir = r'D:\Applicazioni\phantomjs-2.1.1-windows\phantomjs-2.1.1-window
 
 print('Script starting. Press Ctrl + C to cancel.')
 
-# Open the webpage
-print('Opening browser and course page...')
 driver = webdriver.PhantomJS(phantomjs_dir)
-driver.get(course_url)
-assert "Lezione" in driver.title
 
-# Find all lessons
-lessons = driver.find_elements_by_partial_link_text('Lezione')
-if len(lessons) > 0:
-    print('Found {} lessons.'.format(len(lessons)))
-else:
-    sys.exit('No lesson found. Exiting.')
+try:
+    open_page(course_url)
+    get_lesson_list()
+except:
+    print('Script execution was not successful.')
+    sys.exit(1)
+
     
 # Extract the links of the lessons
 print('Getting lessons links...')
