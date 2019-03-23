6abc99ef7ed1eb06ccfd39818026c2532dd27f7e
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2019-03-23 13:08:38 +0100

    Updated script

--- download_lessons.py
+++ download_lessons.py
@@ -7,7 +7,7 @@ import urllib.request
 
 args = parser.setup_parser()
 
-# open browser on login page
+# Open browser on login page
 print('Opening browser towards lessons page...')
 driver = webdriver.Firefox(service_log_path='/dev/null')
 driver.implicitly_wait(args.max_wait)  # seconds to wait for page loading on each click
@@ -19,23 +19,23 @@ driver.find_element_by_id('usernamepassword').click()
 
 try:
     driver.find_element_by_link_text('Portale della Didattica').click()
-except common.exceptions.NoSuchElementException:  
+except common.exceptions.NoSuchElementException:
     print('Error. Could not find Portale della Didattica. Check your login info or that Polito servers are running correctly.')
 
-# try closing something obscuring the course link
-try:
-    driver.find_element_by_partial_link_text(args.course).click()
-except common.exceptions.ElementClickInterceptedException:
-    driver.find_element_by_link_text('Non mostrare più per oggi').click()
-    driver.find_element_by_partial_link_text(args.course).click()
+# Go to course page
+driver.find_element_by_partial_link_text(args.course).click()
 
-driver.find_element_by_partial_link_text('Accedi al materiale e-learning').click()
+# Select "Materiale" tab
 try:
-    driver.find_element_by_partial_link_text('Lezioni Online').click()
+    driver.find_element_by_partial_link_text('Materiale').click()
 except common.exceptions.NoSuchElementException:
-    driver.find_element_by_partial_link_text('Lessons').click()
+    driver.find_element_by_class_name('navbar-toggle').click()
+    driver.find_element_by_partial_link_text('Materiale').click()
 
-# get lessons list
+# Select online lessons page
+driver.find_element_by_xpath('/html/body/div[6]/div/div/div[2]/div/div[2]/angular-filemanager/div/div[2]/div/div[1]/ul/li/ul/li[2]/a').click()
+
+# Get lessons list
 print('Getting lessons list...')
 lessons = driver.find_elements_by_partial_link_text('Lezione')
 print('  Found {} lessons.'.format(len(lessons)))
@@ -52,20 +52,20 @@ else:
 
 print('  Will download {} lessons.'.format(len(urls)))
 
-# get download links
+# Get download links
 download_urls = []
 for item in urls:
-    print('Getting download link of lesson {} of {}...'.format(urls.index(item) + 1, len(urls)))
+    print('Getting download link of lesson {}/{}...'.format(urls.index(item) + 1, len(urls)))
     driver.get(item)
-    download_urls.append(driver.find_element_by_id('video1').get_attribute('href'))
+    download_urls.append(driver.find_element_by_link_text('Video').get_attribute('href'))
 
-# download lessons
+# Download lessons
 for item in download_urls:
-    print('Downloading lesson {} of {}'.format(download_urls.index(item) + 1, len(download_urls)))
+    print('Downloading lesson {}/{}'.format(download_urls.index(item) + 1, len(download_urls)))
     actual_url = urllib.request.urlopen(item).geturl()  # follow redirect to actual file
     with urllib.request.urlopen(actual_url) as response, open('{}/{}'.format(args.output_dir, actual_url.split('/')[-1]), 'wb') as out_file:
         out_file.write(response.read())
 print('All downloads completed successfully!')
 
-# clean up
+# Clean up
 driver.close()
